dd4a68576f2e052b7186643f2de6eac491a18929
KauaResende <godinhokaua@gmail.com>
2023-10-19 11:35:02 -0300

    kakakakaka

--- public/index.php
+++ public/index.php
@@ -65,13 +65,33 @@
         </section>
 
         <section id="season">
-            <div class="left"></div>
-            <div class="right"></div>
+            <div class="left">
+                <img src="https://i.ibb.co/98ztnB1/team-photo-1.png" alt="">
+            </div>
+            <div class="right">
+               <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse cumque 
+                voluptatem pariatur odio! In recusandae nostrum amet dolor dolorem
+                 magni est modi, omnis quasi, non officiis fuga, vel eaque inventore.</p>
+            </div>
         </section>
 
         <section id="first">
-            <div class="left"></div>
-            <div class="right"></div>
+            <div class="left">
+                <img src="https://i.ibb.co/WDVmcfn/first.png" alt="">
+            </div>
+            <div class="right">
+                <p>Vorem ipsum dolor sit amet, consectetur adipiscing elit.
+                     Etiam eu turpis molestie, dictum est a, mattis tellus.
+                    Sed dignissim, metus nec fringilla accumsan, risus sem 
+                  sollicitudin lacus, ut interdum tellus elit sed risus.
+                  Maecenas eget condimentum velit, sit amet feugiat lectus.
+                Class aptent taciti sociosqu ad litora torquent per
+                 conubia nostra, per inceptos himenaeos. Praesent 
+             auctor purus luctus enim egestas, ac scelerisque ante
+              pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus
+              nisl, eu tempor urna. Curabitur vel bibendum lorem.
+             Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.</p>
+            </div>
         </section>
 <!-- Fim do body -->
     <footer>
